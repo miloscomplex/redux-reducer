53fb6f0a6305bafc20c0693e026f3aceda711381
Michael Sutton <hicallmesutton@gmail.com>
2021-03-11 00:51:33 -0600

    commit tree 2ec694f6940d43d2612162ac658a5f7788b2abd5

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [] }, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return { friends: [...state.friends, action.friend] }
+    case 'REMOVE_FRIEND':
+      const friends = state.friends.filter(friend => friend.id !== action.id )
+      return { friends }
+    default:
+      return state 
+  }
 }
